d4c2b6c927c5247c5c618823aed3a2ff0f54a272
Romel Campbell <romel.campbell@regeneron.com>
2015-12-25 05:51:59 -0500

    WIP

---
+++ reddit-clone/app/assets/frontend/helloWorld.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../../DefinitelyTyped/angular2/angular2-2.0.0-alpha.26.d.ts"/>
+
+
+import 'reflect-metadata';
+import {
+  Component
+} from 'angular2/core';
+
+
+@Component({
+  selector: 'hello-world',
+  template: `<div>Hello World</div>`
+})
+export class HelloWorld {
+
+}
--- reddit-clone/app/assets/frontend/main.ts
+++ reddit-clone/app/assets/frontend/main.ts
@@ -1,7 +1,16 @@
+/// <reference path="../../../DefinitelyTyped/angular2/angular2-2.0.0-alpha.26.d.ts"/>
 import Greeter from './greeter';
+import {HelloWorld} from './helloWorld';
+
+import {
+  bootstrap
+} from 'angular2/platform/browser';
 
 let greeter = new Greeter;
 
 let message = greeter.notify('hello');
+
+
+bootstrap(HelloWorld);
 
 alert(message.alertMessage);
