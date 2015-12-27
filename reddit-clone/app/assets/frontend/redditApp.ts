1283142cbe7488783d41b5facf40b3b4948d25c9
Romel Campbell <romel.campbell@regeneron.com>
2015-12-26 20:10:26 -0500

    The basic app is finished. How ever the binding for the votes count is not refreshing .

--- reddit-clone/app/assets/frontend/redditApp.ts
+++ reddit-clone/app/assets/frontend/redditApp.ts
@@ -6,8 +6,12 @@ import {
   Component
 } from 'angular2/core';
 
+import RedditArticle from './redditArticle';
+
+import {Article} from './redditArticle';
 
 @Component({
+  directives: [RedditArticle],
   selector: 'reddit',
   template: `
     <section class="new-link">
@@ -19,13 +23,26 @@ import {
         <div><label for="link">Link:</label></div>
         <div><input #newlink name="link"></div>
       </div>
-      <button (click)="addArticle(newtitle.value,newlink.value)">Submit link</button>
+      <button (click)="addArticle(newtitle,newlink)">Submit link</button>
     </section>
+    <reddit-article *ngFor="#article of articles" [article]="article"></reddit-article>
     `
 })
 export class RedditApp {
-  addArticle(title: string, link: string): void {
-    console.log('Adding article with title', title,
-                'and link ', link ) ;
+  articles: Array<Article>;
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.10'),
+      new Article('Fullstack', 'http://fullstack.io')
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+    this.articles.push(new Article(title.value, link.value));
+    console.log('Adding article with title', title.value,
+                'and link ', link.value ) ;
+    title.value = '';
+    link.value = '';
   }
 }
---
+++ reddit-clone/app/assets/frontend/redditArticle.ts
@@ -0,0 +1,61 @@
+/// <reference path="../../../DefinitelyTyped/angular2/angular2-2.0.0-alpha.26.d.ts"/>
+
+
+import 'reflect-metadata';
+import {
+  Component, View
+} from 'angular2/core';
+
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string) {
+    this.title = title;
+    this.link = link;
+    this.votes = 0;
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+  }
+}
+
+@Component ({
+  selector: 'reddit-article',
+  properties: ['article']
+})
+@View ({
+  template: `
+      <article>
+        <div class="votes">{{ article.votes }}</div>
+        <div class="main">
+          <h2>
+            <a href="{{ article.link }}">{{ article.title }}</a>
+          </h2>
+          <ul>
+            <li><a href (click)="voteUp()">upvote</a></li>
+            <li><a href (click)="voteDown()">downvote</a></li>
+          </ul>
+        </div>
+      </article>
+  `
+})
+export default class RedditArticle {
+  article: Article;
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+}
