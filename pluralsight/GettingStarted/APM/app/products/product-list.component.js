86851f84c288eb86cb1663eab5094ea34b85a7f4
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-06-05 05:52:59 -0400

    Done with 'Data binding and pipe' module.

--- pluralsight/GettingStarted/APM/app/products/product-list.component.js
+++ pluralsight/GettingStarted/APM/app/products/product-list.component.js
@@ -21,6 +21,10 @@ System.register(["angular2/core"], function(exports_1, context_1) {
             ProductListComponent = (function () {
                 function ProductListComponent() {
                     this.pageTitle = "Product List";
+                    this.imageWidth = 50;
+                    this.imageMargin = 2;
+                    this.showImage = false;
+                    this.listFilter = "cart";
                     this.products = [
                         {
                             "productId": 2,
@@ -44,6 +48,9 @@ System.register(["angular2/core"], function(exports_1, context_1) {
                         }
                     ];
                 }
+                ProductListComponent.prototype.toggleImage = function () {
+                    this.showImage = !this.showImage;
+                };
                 ProductListComponent = __decorate([
                     core_1.Component({
                         selector: "pm-products",
