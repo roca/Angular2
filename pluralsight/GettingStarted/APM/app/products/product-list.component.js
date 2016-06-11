8240d9ac040204e1acd3ec61dc5afb8c35f606e6
Romel Campbell <romel.campbell@regeneron.com>
2016-06-11 05:57:44 -0400

    Adds a service

--- pluralsight/GettingStarted/APM/app/products/product-list.component.js
+++ pluralsight/GettingStarted/APM/app/products/product-list.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "./product-filter.pipe", "../shared/star.componenet"], function(exports_1, context_1) {
+System.register(["angular2/core", "./product-filter.pipe", "../shared/star.componenet", "./product.service"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(["angular2/core", "./product-filter.pipe", "../shared/star.compo
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, product_filter_pipe_1, star_componenet_1;
+    var core_1, product_filter_pipe_1, star_componenet_1, product_service_1;
     var ProductListComponent;
     return {
         setters:[
@@ -22,43 +22,24 @@ System.register(["angular2/core", "./product-filter.pipe", "../shared/star.compo
             },
             function (star_componenet_1_1) {
                 star_componenet_1 = star_componenet_1_1;
+            },
+            function (product_service_1_1) {
+                product_service_1 = product_service_1_1;
             }],
         execute: function() {
             ProductListComponent = (function () {
-                function ProductListComponent() {
+                function ProductListComponent(_productService) {
+                    this._productService = _productService;
                     this.pageTitle = "Product List";
                     this.imageWidth = 50;
                     this.imageMargin = 2;
                     this.showImage = false;
-                    this.listFilter = "cart";
-                    this.products = [
-                        {
-                            "productId": 2,
-                            "productName": "Garden Cart",
-                            "productCode": "GDN-0023",
-                            "releaseDate": "March 18, 2016",
-                            "description": "15 gallon capacity rolling garden cart",
-                            "price": 32.99,
-                            "starRating": 4.2,
-                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-                        },
-                        {
-                            "productId": 5,
-                            "productName": "Hammer",
-                            "productCode": "TBX-0048",
-                            "releaseDate": "May 21, 2016",
-                            "description": "Curved claw steel hammer",
-                            "price": 8.9,
-                            "starRating": 4.8,
-                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-                        }
-                    ];
                 }
                 ProductListComponent.prototype.toggleImage = function () {
                     this.showImage = !this.showImage;
                 };
                 ProductListComponent.prototype.ngOnInit = function () {
-                    console.log("In OnInit");
+                    this.products = this._productService.getProducts();
                 };
                 ProductListComponent.prototype.onRatingClicked = function (message) {
                     this.pageTitle = "Product List " + message;
@@ -71,7 +52,7 @@ System.register(["angular2/core", "./product-filter.pipe", "../shared/star.compo
                         pipes: [product_filter_pipe_1.ProductFilterPipe],
                         directives: [star_componenet_1.StarComponent]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [product_service_1.ProductService])
                 ], ProductListComponent);
                 return ProductListComponent;
             }());
---
+++ pluralsight/GettingStarted/APM/app/products/product.service.js
@@ -0,0 +1,58 @@
+System.register(["angular2/core"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var ProductService;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            ProductService = (function () {
+                function ProductService() {
+                }
+                ProductService.prototype.getProducts = function () {
+                    return [
+                        {
+                            "productId": 2,
+                            "productName": "Garden Cart",
+                            "productCode": "GDN-0023",
+                            "releaseDate": "March 18, 2016",
+                            "description": "15 gallon capacity rolling garden cart",
+                            "price": 32.99,
+                            "starRating": 4.2,
+                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+                        },
+                        {
+                            "productId": 5,
+                            "productName": "Hammer",
+                            "productCode": "TBX-0048",
+                            "releaseDate": "May 21, 2016",
+                            "description": "Curved claw steel hammer",
+                            "price": 8.9,
+                            "starRating": 4.8,
+                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+                        }
+                    ];
+                };
+                ProductService = __decorate([
+                    core_1.Injectable(), 
+                    __metadata('design:paramtypes', [])
+                ], ProductService);
+                return ProductService;
+            }());
+            exports_1("ProductService", ProductService);
+        }
+    }
+});
+//# sourceMappingURL=product.service.js.map
