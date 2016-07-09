0a72f19478498e9db85a019c41c8c312cd23ad91
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-07-09 06:00:28 -0400

    Finished: Components and Data Binding

--- pluralsight/pypAngular2Quick/pbp-a2-ward/app/app.component.ts
+++ pluralsight/pypAngular2Quick/pbp-a2-ward/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CustomerComponent } from './customer/customer.component';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+    directives: [ CustomerComponent ]
 })
 export class AppComponent {
     // [ attribute/property ] means property binding C to D
@@ -10,9 +12,16 @@ export class AppComponent {
 
     title = 'Customer App';
     name = 'Ward';
-    wardsColor = 'blue'; // just like his suit
+    wardsColor = 'green'; // just like his suit
+    customers = [
+      {id: 1, name: 'Ward'},
+      {id: 2, name: 'Kevin'},
+      {id: 3, name: 'Eric'},
+      {id: 4, name: 'Sally'},
+      {id: 5, name: 'Emmet'},
+    ];
 
     changeSuitColor() {
-      this.wardsColor =  this.wardsColor === 'blue' ? 'red' : 'blue';
+      this.wardsColor =  this.wardsColor === 'green' ? 'red' : 'green';
     }
 }
---
+++ pluralsight/pypAngular2Quick/pbp-a2-ward/app/customer/customer.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-customer', // <app-customer>
+    templateUrl: 'app/customer/customer.component.html'
+})
+export class CustomerComponent implements OnInit {
+  @Input() customer: {id: number, name: string};
+
+  constructor() {}
+  ngOnInit() {}
+}
